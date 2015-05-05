347fdc7ab76e9bfa005ab281dd29b380d46def47
jani2gopal <jani2gopal@gmail.com>
2015-05-04 20:18:44 -0400

    Flocking F16, Bird and Heli
    experimental algorithm known as boids is an artificial life program,
    developed by Craig Reynolds in 1986, using which we can simulate the
    flying patterns of the bird.
    
    The aim of the research was to simulate and understand complex system in
    nature i.e. boids algorithm (flocking bird) to prove the efficiency and
    open a direction to implementing it in drones.
    To achieve the research aim, boids algorithm of animal movements was
    simulated for different aircrafts and flock of bird, which proves the
    potential of the boids algorithm and its usability in autonomous flying
    drones.

---
+++ Assets/Flocking/Boid Controller.js
@@ -0,0 +1,22 @@
+// these define the flock's behaviorvar minVelocity : float 	= 5;var maxVelocity : float 	= 20;
+var randomness : float 		= 1; var flockSize : int			= 20;
+var prefab : GameObject; 
+var chasee : GameObject;
+
+var flockCenter : Vector3;
+var flockVelocity : Vector3;
+
+private var f16as;
+function Start() {
+	f16as = new Array(flockSize);
+	for (var i=0; i<flockSize; i++) {		var position = Vector3(						Random.value*collider.bounds.size.x,						Random.value*collider.bounds.size.y,						1)-collider.bounds.extents;		var f16a = Instantiate(prefab, transform.position, transform.rotation);		f16a.transform.parent = transform;		f16a.transform.localPosition = position;
+		f16a.GetComponent("Boid Flocking").setController(gameObject);
+		f16as[i] = f16a;
+	}}function Update () {   
+   	var theCenter = Vector3.zero;   	var theVelocity = Vector3.zero;
+   	for (var f16a : GameObject in f16as) {
+		theCenter       = theCenter + f16a.transform.localPosition;		theVelocity     = theVelocity + f16a.rigidbody.velocity;
+   	}
+	flockCenter = theCenter/(flockSize);	
+	flockVelocity = theVelocity/(flockSize);
+}
